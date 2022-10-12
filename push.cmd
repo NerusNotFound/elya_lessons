ce2fe1942ca98d5c3cd19d5a71066b860cc20cbf
khe4oyan <khe4oyan@bk.ru>
2022-10-12 21:41:21 +0400

    .

--- push.cmd
+++ push.cmd
@@ -2,6 +2,6 @@ git s
 pause
 git add . 
 git commit -m "."
-git push https://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@github.com/NerusNotFound/yt_page.git
+git push https://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@github.com/NerusNotFound/elya_lessons.git
 echo done
 pause
