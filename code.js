8cb744385ba882b41ea09fd1d3926bde04c1803f
khe4oyan <khe4oyan@bk.ru>
2022-10-12 21:50:57 +0400

    .

---
+++ code.js
@@ -0,0 +1,10 @@
+const lessons = [
+	{ week_day: 0, hour_start: null, hour_finish: null, less_name: null	},
+];
+
+const today = new Date();
+const week_day_now = today.getDay();
+const hours_now = today.getHours();
+const minute_now = today.getMinutes();
+
+
