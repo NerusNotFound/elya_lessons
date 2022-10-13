20b46e06dfc9b8b3d90ed2d85c6764663ca191fb
NerusNotFound <115404607+NerusNotFound@users.noreply.github.com>
2022-10-13 23:21:54 +0400

    fix logo

--- code.js
+++ code.js
@@ -13,60 +13,63 @@ class Clock {
 
 const lessons = [
 	[
-		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 7113, teacher: 'Գալոլոյան Գ' },
-		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 7113, teacher: 'Գալոլոյան Գ' },
-		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 45), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 7113, teacher: 'Գալոլոյան Գ' },
-		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Կառավարչական հաշվառում', lessons_number: 2431, teacher: 'Բարիկյան' },
-		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Կառավարչական հաշվառում', lessons_number: 2431, teacher: 'Բարիկյան' },
-		{ start_at: new Clock(17, 30), finish_at: new Clock(18, 15), less_name: 'Կառավարչական հաշվառում', lessons_number: 2431, teacher: 'Բարիկյան' },
+		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 7113, teacher: 'Գալոլոյան Գ', less_char: 'Գ' },
+		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 7113, teacher: 'Գալոլոյան Գ', less_char: 'Գ' },
+		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 45), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 7113, teacher: 'Գալոլոյան Գ', less_char: 'Գ' },
+		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Կառավարչական հաշվառում', lessons_number: 2431, teacher: 'Բարիկյան', less_char: 'Դ' },
+		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Կառավարչական հաշվառում', lessons_number: 2431, teacher: 'Բարիկյան', less_char: 'Դ/Գ' },
+		{ start_at: new Clock(17, 30), finish_at: new Clock(18, 15), less_name: 'Կառավարչական հաշվառում', lessons_number: 2431, teacher: 'Բարիկյան', less_char: 'Գ' },
 	],
 
 	[
-		{ start_at: new Clock(9, 30), finish_at: new Clock(10, 15), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 2030, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(10, 20), finish_at: new Clock(11, 5), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 2030, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(11, 15), finish_at: new Clock(12, 0), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(12, 5), finish_at: new Clock(12, 50), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.' },
-		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.' },
-		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.' },
+		{ start_at: new Clock(9, 30), finish_at: new Clock(10, 15), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 2030, teacher: 'Աղաջանյան Հ.', less_char: 'Դ' },
+		{ start_at: new Clock(10, 20), finish_at: new Clock(11, 5), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 2030, teacher: 'Աղաջանյան Հ.', less_char: 'Դ' },
+		{ start_at: new Clock(11, 15), finish_at: new Clock(12, 0), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.', less_char: 'Դ/Գ' },
+		{ start_at: new Clock(12, 5), finish_at: new Clock(12, 50), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.', less_char: 'Դ/Գ' },
+		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.', less_char: 'Գ' },
+		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Հարկեր, վճարներ, տուրքեր ', lessons_number: 3102, teacher: 'Աղաջանյան Հ.', less_char: 'Գ' },
+		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.', less_char: 'Դ' },
+		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.', less_char: 'Դ' },
+		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.', less_char: 'Դ' },
 	],
 
 	[
-		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան' },
-		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան' },
-		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան' },
-		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան' },
-		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 2418, teacher: 'Գալոյան Գ.' },
-		{ start_at: new Clock(17, 30), finish_at: new Clock(18, 15), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 2418, teacher: 'Գալոյան Գ.' },
-		{ start_at: new Clock(18, 25), finish_at: new Clock(19, 10), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 2418, teacher: 'Գալոյան Գ.' },
+		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան', less_char: 'Դ' },
+		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան', less_char: 'Դ' },
+		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան', less_char: 'Գ' },
+		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Շինարարական արտադրության տեխնոլոգիա և կազմակերպում', lessons_number: 2116, teacher: 'Ղուլյան', less_char: 'Գ' },
+		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 2418, teacher: 'Գալոյան Գ.', less_char: 'Գ' },
+		{ start_at: new Clock(17, 30), finish_at: new Clock(18, 15), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 2418, teacher: 'Գալոյան Գ.', less_char: 'Գ' },
+		{ start_at: new Clock(18, 25), finish_at: new Clock(19, 10), less_name: 'Գնագոյացումը շինարարության ոլորտում', lessons_number: 2418, teacher: 'Գալոյան Գ.', less_char: 'Գ' },
 	],
 
 	[
-		{ start_at: new Clock(12, 5), finish_at: new Clock(12, 50), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 17302, teacher: 'Եդոյան' },
-		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 17302, teacher: 'Եդոյան' },
-		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 17302, teacher: 'Եդոյան' },
-		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.' },
-		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.' },
-		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.' },
+		{ start_at: new Clock(12, 5), finish_at: new Clock(12, 50), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 17302, teacher: 'Եդոյան', less_char: 'Դ' },
+		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 17302, teacher: 'Եդոյան', less_char: 'Դ' },
+		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 17302, teacher: 'Եդոյան', less_char: 'Դ' },
+		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.', less_char: 'Գ' },
+		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.', less_char: 'Գ' },
+		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Մարքեթինգ', lessons_number: 2429, teacher: 'Բեգլարյան Ա.', less_char: 'Գ' },
 	],
 
 	[
-		{ start_at: new Clock(11, 15), finish_at: new Clock(12, 0), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 2241, teacher: 'Եդոյան Տ.' },
-		{ start_at: new Clock(12, 5), finish_at: new Clock(12, 50), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 2241, teacher: 'Եդոյան Տ.' },
-		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.' },
-		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.' },
+		{ start_at: new Clock(11, 15), finish_at: new Clock(12, 0), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 2241, teacher: 'Եդոյան Տ.', less_char: 'Գ' },
+		{ start_at: new Clock(12, 5), finish_at: new Clock(12, 50), less_name: 'Վիճակագրության տեսության հիմունքները', lessons_number: 2241, teacher: 'Եդոյան Տ.', less_char: 'Գ' },
+		{ start_at: new Clock(13, 10), finish_at: new Clock(13, 55), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.', less_char: 'Դ' },
+		{ start_at: new Clock(14, 0), finish_at: new Clock(14, 45), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.', less_char: 'Դ' },
+		{ start_at: new Clock(14, 55), finish_at: new Clock(15, 40), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.', less_char: 'Դ' },
+		{ start_at: new Clock(15, 45), finish_at: new Clock(16, 30), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.', less_char: 'Գ' },
+		{ start_at: new Clock(16, 40), finish_at: new Clock(17, 25), less_name: 'Լոգիստիկա', lessons_number: 2107, teacher: 'Աղաջանյան Հ.', less_char: 'Գ' },
 	],
 	];
 
 const today = new Date();
 const week_day_now = today.getDay() - 1;
-const hours_now = today.getHours();
-const minute_now = today.getMinutes();
+// const hours_now = today.getHours();
+// const minute_now = today.getMinutes();
+
+const hours_now = 12;
+const minute_now = 30;
 
 
 console.log(`Time: ${hours_now}:${minute_now}`);
@@ -158,8 +161,8 @@ function turn_show(lessons_elem) {
 
 function create_block(less_elem, list) {
 	less_elem.appendChild(inner_p(list.less_name, ['item']));
-	less_elem.appendChild(inner_p('Դասախոս։ ' + list.teacher));
-	less_elem.appendChild(inner_p('Լսարան։ ' + list.lessons_number));
+	less_elem.appendChild(inner_p(`Դասախոս։ ${list.teacher}`));
+	less_elem.appendChild(inner_p(`Լսարան։ ${list.lessons_number} (${list.less_char})`));
 
 	const start_min = min_00(list.start_at.get_min());
 	const finish_min = min_00(list.finish_at.get_min());
